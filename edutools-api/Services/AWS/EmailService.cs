ac8dbeee48afd9a0f1efc068dcfa74a2b61e2b36
Ariel Pérez <ariel.perez@userena.cl>
2023-04-01 21:11:04 -0300

    Se modifica funcionamiento de cliente SMTP

--- edutools-api/Services/AWS/EmailService.cs
+++ edutools-api/Services/AWS/EmailService.cs
@@ -11,22 +11,29 @@ namespace edutools_api.Services.AWS
     public class EmailService : IEmailService
     {
         private readonly IConfiguration _Configuration;
+        private readonly string SMTPHost;
+        private readonly string SMTPUsername;
+        private readonly string SMTPPassword;
+        private readonly int SMTPPort;
+        private readonly SmtpClient _SmtpClient;
 
         public EmailService(IConfiguration config)
         {
             _Configuration = config;
+            var smtpSection = _Configuration.GetSection("SmtpCredentials");
+            SMTPHost = smtpSection.GetValue<string>("Host");
+            SMTPUsername = smtpSection.GetValue<string>("Username");
+            SMTPPassword = smtpSection.GetValue<string>("Password");
+            SMTPPort = smtpSection.GetValue<int>("Port");
+            _SmtpClient = new SmtpClient(SMTPHost, SMTPPort);
+            _SmtpClient.UseDefaultCredentials = false;
+            _SmtpClient.Credentials = new NetworkCredential(SMTPUsername, SMTPPassword);
+            _SmtpClient.EnableSsl = true;
+
         }
 
         public async Task<bool> SendEmail(string to, string body)
         {
-            string host = "email-smtp.us-east-1.amazonaws.com";
-            int port = 587;
-            string smtpUser = "";
-            string smtpPassword = "";
-            SmtpClient client = new SmtpClient(host, port);
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential(smtpUser, smtpPassword);
-            client.EnableSsl = true;
             MailAddress from = new MailAddress("aperezf91@gmail.com", "Hola1", Encoding.UTF8);
             MailAddress toA = new MailAddress(to, to, Encoding.UTF8);
             MailMessage message = new MailMessage(from, toA);
@@ -35,7 +42,7 @@ namespace edutools_api.Services.AWS
             message.Subject = "Hola2";
             message.SubjectEncoding = Encoding.UTF8;
 
-            await client.SendMailAsync(message);
+            await _SmtpClient.SendMailAsync(message);
             return true;
 
         }
